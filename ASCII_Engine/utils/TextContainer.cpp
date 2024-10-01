d4457a1bb95bb252a62c207bc84b6894177d7c1c
Victor A. P. Oliveira <victorandreoliveira@gmail.com>
2024-10-01 15:27:35 -0300

    :construction: fix:TextContainer

--- ASCII_Engine/utils/TextContainer.cpp
+++ ASCII_Engine/utils/TextContainer.cpp
@@ -19,21 +19,21 @@ std::ostream &operator<<(std::ostream &out, const TextContainer &tc)
 
 void TextContainer::addText(std::string title, std::string text)
 {
-	textMap[title] = (text + "\n");
+	textMap[title] = text;
 }
 
 void TextContainer::addFromFile(std::string nameFile, std::string title)
 {
 	std::ifstream fsprt(nameFile.c_str(),std::ios::in);
 	
-	std::string line, textTitle = "_NONE_";
+	std::string line, textTitle = "empty";
 	bool textFlag = false;
 	
 	while (getline(fsprt,line))
 	{
 		if (line.starts_with(title))
 			textFlag = false;
-		else if (textTitle == "_NONE_")
+		else if (textTitle == "empty")
 			throw std::runtime_error("TextContainer: Arquivo mal formatado. Faltou titulo.");
 		
 		if (textFlag)
@@ -42,11 +42,14 @@ void TextContainer::addFromFile(std::string nameFile, std::string title)
 		}
 		else	//tratando título
 		{
+			textMap[textTitle].erase(textMap[textTitle].find_last_not_of(" \t\n") + 1);
 			line.erase(0,title.length());
 			textTitle = line.substr(line.find_first_not_of(" \t"));
 			textFlag = true;
 		}
 	}
+	
+	textMap[textTitle].erase(textMap[textTitle].find_last_not_of(" \t\n") + 1);
 	
 	fsprt.close();
 }
